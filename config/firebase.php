1af3f781d432683130e521fdbff2ce9df400516d
Minh Nhut <lenhut0929@gmail.com>
2024-12-18 09:46:08 +0700

    update

--- config/firebase.php
+++ config/firebase.php
@@ -2,8 +2,12 @@
 
 return [
 
+    // 'credentials' => [
+    //     'file' => storage_path('app/firebase/rfid-7f3b2-firebase-adminsdk-sf2b5-ff8731ee74.json'),
+    // ],
+
     'credentials' => [
-        'file' => storage_path('app/firebase/rfid-7f3b2-firebase-adminsdk-sf2b5-ff8731ee74.json'),
+        'json' => json_decode(env('FIREBASE_CREDENTIALS_JSON'), true),
     ],
 
     'storage' => [
