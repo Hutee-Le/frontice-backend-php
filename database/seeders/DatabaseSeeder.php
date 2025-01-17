42734e1ed78e732a9d193a89a0c81d52577a764a
Minh Nhut <lenhut0929@gmail.com>
2025-01-17 10:23:04 +0700

    update DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,12 +16,12 @@ class DatabaseSeeder extends Seeder
         $this->call(LevelSeeder::class);
         $this->call(AdminSeeder::class);
         $this->call(ServiceSeeder::class);
-        $this->call([
-            TaskeeSeeder::class,           // Tạo 10 Taskee
-            TaskerSeeder::class,        // Tạo 10 Tasker
-            ChallengeSeeder::class,        // Tạo 20 Challenge
-            ChallengeSolutionSeeder::class, // Tạo 30 Challenge Solutions
-            TaskSeeder::class,           // Tạo 100 Task
-        ]);
+        // $this->call([
+        //     TaskeeSeeder::class,           // Tạo 10 Taskee
+        //     TaskerSeeder::class,        // Tạo 10 Tasker
+        //     ChallengeSeeder::class,        // Tạo 20 Challenge
+        //     ChallengeSolutionSeeder::class, // Tạo 30 Challenge Solutions
+        //     TaskSeeder::class,           // Tạo 100 Task
+        // ]);
     }
 }
